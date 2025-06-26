6d927e011aa6e3b8efb1430aa947963954805a98
Hagen <ceceba223@users.noreply.github.com>
2025-06-26 18:06:07 +0200

    Dip Switch calc working

--- dip-switch.js
+++ dip-switch.js
@@ -2,8 +2,34 @@ const inputField = document.getElementById('input-address');
 const outputField = document.getElementById('output-address');
 const form = document.getElementById('converter');
 
+const switch1 = document.getElementById('switch-1');
+const switch2 = document.getElementById('switch-2');
+const switch3 = document.getElementById('switch-3');
+const switch4 = document.getElementById('switch-4');
+const switch5 = document.getElementById('switch-5');
+const switch6 = document.getElementById('switch-6');
+const switch7 = document.getElementById('switch-7');
+const switch8 = document.getElementById('switch-8');
+const switch9 = document.getElementById('switch-9');
+const switches = [switch1, switch2, switch3, switch4, switch5, switch6, switch7, switch8, switch9];
+
+
 function convertToBin(inputAddress) {
     return inputAddress.toString(2).padStart(9, '0');
+}
+
+function setSwitches(binAddress) {
+    for (let i = 0; i < 9; i += 1) {
+        currentSwitch = switches[i];
+        console.log(i);
+        if (binAddress[9-i-1] == 1){
+            //console.log(binAddress[9-i])
+            currentSwitch.style.top = '31%';
+        } else {
+            //console.log(binAddress[9-i])
+            currentSwitch.style.top = '57%';
+        }
+    }
 }
 
 form.addEventListener('input', () => {
@@ -14,5 +40,6 @@ form.addEventListener('input', () => {
     }
 
     const outputAddress = convertToBin(inputAddress);
-    outputField.value = outputAddress;
+    setSwitches(outputAddress);
+    //outputField.value = outputAddress;
 });
